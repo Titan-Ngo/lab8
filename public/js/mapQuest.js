7ba366932f5d11224fb0ea505ae7592547fda395
Titan-Ngo <ttngo@ucsd.edu>
2020-02-27 01:49:25 -0800

    lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'zQ9dpsKVecGEYSzYNAC4dC352JANtrL6';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	  center: [32.88, -117.236],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12,
+	});
+
+	// Add a marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -2,9 +2,27 @@ var data = {
   rsvp: ['ixd@ucsd.edu']
 };
 
+
+
 /*
  * GET tasks page.
  */
 exports.adminView = function(request, response){
   response.render('rsvp', data);
+};
+
+
+
+exports.addRSVP = function(request, response) {
+var rsvpEmail = request.body.rsvpEmail;
+
+  // print to the terminal whenever the POSt reuqest is made
+  console.log(rsvpEmail);
+
+
+  data.rvsp.push(rsvpEmail); // add to curr data
+
+  // Let us send back some data so that
+  // browser knows it worked!
+  response.send(rsvpEmail);
 };
